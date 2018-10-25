7355db6b1bf8909253a5314a426e70caae45be7c
Danil-k <ooo.kerm@yandex.ru>
2018-10-25 14:41:52 +0300

    7. JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,8 @@
+"use strict";
+
 $(document).ready(function(){
+
+  //SLIDER
 
   var owl = $('.owl-carousel');
   owl.owlCarousel({
@@ -13,6 +17,25 @@ $(document).ready(function(){
 
   $('.slider-prev').click(function() {
     owl.trigger('prev.owl.carousel');
+  });
+
+  // SCROLL MENU
+
+  $('.header__burger').on('click', function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href');
+    var top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 800);
+  });
+
+  //MENU
+
+  $('.menu__list-item__title').on('click', function () {
+    if ($(this).parent().hasClass('menu__list-item_o')) {
+      $(this).parent().addClass('menu__list-item_c').removeClass('menu__list-item_o');
+    } else if ($(this).parent().hasClass('menu__list-item_c')) {
+      $(this).parent().addClass('menu__list-item_o').removeClass('menu__list-item_c');
+    }
   });
 
 });
